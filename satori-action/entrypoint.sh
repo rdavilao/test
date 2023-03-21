10e51d48cfc5a708a5c1198e0f81630d53d9d196
rdavilao <rdavilao@outlook.com>
2023-03-21 23:18:04 +0100

    Update entrypoint.sh

--- satori-action/entrypoint.sh
+++ satori-action/entrypoint.sh
@@ -5,8 +5,6 @@ echo " - format: [$INPUT_FORMAT]"
 
 repo=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)
 
-echo "REPO: $repo"
-
 if [ "$INPUT_FORMAT" = "conga" ]; then
     INPUT_FILES=$repo.xmi
 else
@@ -14,16 +12,12 @@ else
     zip -r $INPUT_FILES .
 fi
 
-ls
-
-echo "INPUT: $INPUT_FILES"
-
 XMI_OUTPUT="${INPUT_FILES/.zip/.xmi}"
 METRICS_OUTPUT="${INPUT_FILES/.zip/.metrics.json}"
 
 if [ "$INPUT_FORMAT" = "conga" ]; then
 #  echo `ls /`
-    ##java -jar /AsymobJSON.jar $INPUT_FILES | tee /tmp/output.txt
+    java -jar /AsymobJSON.jar $INPUT_FILES | tee /tmp/output.txt
     COUNT=`grep "INP = 0" /tmp/output.txt | wc --lines`
     if [ $COUNT -gt 2 ]; then
 	echo "More than two intents with INP > 2"
@@ -31,9 +25,9 @@ if [ "$INPUT_FORMAT" = "conga" ]; then
     fi
 elif [ "$INPUT_FORMAT" = "Dialogflow" ]; then
     echo "::debug::{Running CONGA with $INPUT_FILES}"
-    ##java -jar /CongaReverse.jar $INPUT_FILES $INPUT_FORMAT
+    java -jar /CongaReverse.jar $INPUT_FILES $INPUT_FORMAT
     echo "::debug::{Running Asymob with $XMI_OUTPUT}"
-    ##java -jar /AsymobJSON.jar $XMI_OUTPUT
+    java -jar /AsymobJSON.jar $XMI_OUTPUT
 fi
 
 echo "::group::Metrics"
