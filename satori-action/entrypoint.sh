8acfd7ab985e451be4fe5b9cf419595b7839f49c
Daniel Avila <rdavilao@users.noreply.github.com>
2023-03-21 15:07:38 +0100

    Testing

--- satori-action/entrypoint.sh
+++ satori-action/entrypoint.sh
@@ -1,25 +1,22 @@
 #!/bin/bash 
 
-echo "Testing chat-bot from [$GITHUB_REPOSITORY] with:"
+echo "Testing chat-bot from $GITHUB_REPOSITORY with:"
 echo " - format: [$INPUT_FORMAT]"
 
-echo "CONTENIDO: "
-ls
-
-XMI_OUTPUT="${INPUT_FILES/.zip/.xmi}"
-METRICS_OUTPUT="${INPUT_FILES/.zip/.metrics.json}"
+XMI_OUTPUT="${GITHUB_REPOSITORY_NAME/.zip/.xmi}"
+METRICS_OUTPUT="${GITHUB_REPOSITORY_NAME/.zip/.metrics.json}"
 
 if [ "$INPUT_FORMAT" = "conga" ]; then
 #  echo `ls /`
-    java -jar /AsymobJSON.jar $INPUT_FILES | tee /tmp/output.txt
+    java -jar /AsymobJSON.jar $GITHUB_REPOSITORY_NAME | tee /tmp/output.txt
     COUNT=`grep "INP = 0" /tmp/output.txt | wc --lines`
     if [ $COUNT -gt 2 ]; then
 	echo "More than two intents with INP > 2"
 	exit 1
     fi
 elif [ "$INPUT_FORMAT" = "Dialogflow" ]; then
-    echo "::debug::{Running CONGA with $INPUT_FILES}"
-    java -jar /CongaReverse.jar $INPUT_FILES $INPUT_FORMAT
+    echo "::debug::{Running CONGA with $GITHUB_REPOSITORY_NAME}"
+    java -jar /CongaReverse.jar $GITHUB_REPOSITORY_NAME $INPUT_FORMAT
     echo "::debug::{Running Asymob with $XMI_OUTPUT}"
     java -jar /AsymobJSON.jar $XMI_OUTPUT
 fi
