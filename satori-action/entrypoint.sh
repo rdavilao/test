e39641d30fd8599ea98b87114f9c7ecb588e1bf9
Daniel Avila <rdavilao@users.noreply.github.com>
2023-03-21 15:09:36 +0100

    Update entrypoint.sh

--- satori-action/entrypoint.sh
+++ satori-action/entrypoint.sh
@@ -8,7 +8,7 @@ METRICS_OUTPUT="${GITHUB_REPOSITORY_NAME/.zip/.metrics.json}"
 
 if [ "$INPUT_FORMAT" = "conga" ]; then
 #  echo `ls /`
-    java -jar /AsymobJSON.jar $GITHUB_REPOSITORY_NAME | tee /tmp/output.txt
+    java -jar /AsymobJSON.jar $GITHUB_REPOSITORY_NAME.xmi | tee /tmp/output.txt
     COUNT=`grep "INP = 0" /tmp/output.txt | wc --lines`
     if [ $COUNT -gt 2 ]; then
 	echo "More than two intents with INP > 2"
@@ -16,7 +16,7 @@ if [ "$INPUT_FORMAT" = "conga" ]; then
     fi
 elif [ "$INPUT_FORMAT" = "Dialogflow" ]; then
     echo "::debug::{Running CONGA with $GITHUB_REPOSITORY_NAME}"
-    java -jar /CongaReverse.jar $GITHUB_REPOSITORY_NAME $INPUT_FORMAT
+    java -jar /CongaReverse.jar $GITHUB_REPOSITORY_NAME.zip $INPUT_FORMAT
     echo "::debug::{Running Asymob with $XMI_OUTPUT}"
     java -jar /AsymobJSON.jar $XMI_OUTPUT
 fi
