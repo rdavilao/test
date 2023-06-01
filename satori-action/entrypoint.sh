7570b5aad9cbcd20295d98627e6e2d8fac40e07d
rdavilao <rdavilao@outlook.com>
2023-06-01 18:46:04 +0200

    Update entrypoint.sh

--- satori-action/entrypoint.sh
+++ satori-action/entrypoint.sh
@@ -557,6 +557,7 @@ if [ "$INPUT_FORMAT" = "conga" ]; then
     fi
 else
     echo "::debug::{Running CONGA with $INPUT_FILES}"
+    echo "Version $INPUT_VERSION"
     ls
     java -jar /CongaReverse2.0_pUML.jar $INPUT_FILES $INPUT_FORMAT $INPUT_VERSION
 
